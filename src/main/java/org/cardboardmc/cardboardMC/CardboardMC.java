b519ba32b22e6498f96a3610ed937de6b0ccab48
Samma - PixelStudio <81291107+Samma2009@users.noreply.github.com>
2025-06-27 17:30:58 +0200

    create

---
+++ src/main/java/org/cardboardmc/cardboardMC/CardboardMC.java
@@ -0,0 +1,17 @@
+package org.cardboardmc.cardboardMC;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class CardboardMC extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
